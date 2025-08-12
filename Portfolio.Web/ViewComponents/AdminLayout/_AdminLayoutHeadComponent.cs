262a08eb7789c045f56b75b0d9d8bc5ddec48b3a
erhangndz <erhangndz@live.com>
2025-08-12 22:25:17 +0300

    updated

---
+++ Portfolio.Web/ViewComponents/AdminLayout/_AdminLayoutHeadComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Web.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutHeadComponent: ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}

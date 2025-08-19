3e1f018eac309fdb3ab38bcc5001f310349c041c
erhangndz <erhangndz@live.com>
2025-08-19 22:27:12 +0300

    updated

--- Portfolio.Web/Controllers/ProjectController.cs
+++ Portfolio.Web/Controllers/ProjectController.cs
@@ -53,6 +53,37 @@ namespace Portfolio.Web.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult UpdateProject(int id)
+        {
+            CategoryDropdown(); 
+            var project = context.Projects.Find(id);
+            return View(project);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProject(Project model)
+        {
+            CategoryDropdown();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            context.Projects.Update(model);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult DeleteProject(int id)
+        {
+            var project = context.Projects.Find(id);
+            context.Remove(project);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         
     }
 }
